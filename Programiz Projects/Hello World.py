a648bcdb75a2dc1c8ebea8b52fd159641db2eb7a
PythonCodes1 <59237812+PythonCodes1@users.noreply.github.com>
2020-05-05 19:37:53 -0400

    Create Hello World.py

---
+++ Programiz Projects/Hello World.py
@@ -0,0 +1,16 @@
+# This is the beginning of learning a new language and this is the most basic function you can learn from this language or any other language in my opinion
+
+print('Hello World!')
+
+"""
+Output:
+Hello World!
+
+# You can even go as far as paragraphs! But you should use this "\n" command to help you wrap the text.
+print('Lorem ipsum dolor sit amet, consectetur adipiscing elit. \nNam ultricies accumsan ligula ut luctus. Duis laoreet justo nec arcu \imperdiet tristique. Proin et sapien auctor, condimentum velit vel, \ngravida lectus. Maecenas ac orci commodo, interdum turpis mollis, interdum augue. \nMorbi pulvinar posuere enim vel luctus. Ut sit amet scelerisque tortor. Nam vitae eleifend dolor.')
+
+# There are also special characters that usually mean something else in the language of python. 
+# List: \newline & \\ & \' & \" & \a & \b & \f & \n & \r & \t & \v & \ooo & \x**
+# EXTRA: \N{name}: Character named {name} in the Unicode database
+# EXTRA: \uxxxx: Character with 16-bit hex value xxxx. Exactly four hexadecimal digits are required.
+# EXTRA: \Uxxxxxxxx: Character with 32-bit hex value xxxxxxxx. Exactly eight hexadecimal digits are required.

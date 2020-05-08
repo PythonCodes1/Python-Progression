bbbe807d269cc0c5ad6246bf513426999f1bec1e
PythonCodes1 <59237812+PythonCodes1@users.noreply.github.com>
2020-05-08 19:11:36 -0400

    Create Find the Area of a Triangle.py

---
+++ Programiz Projects/Find the Area of a Triangle.py
@@ -0,0 +1,13 @@
+"""
+To find the area of a triangle, you must use this method:
+s = (a+b+c)/2
+area = √(s(s-a)*(s-b)*(s-c))
+"""
+
+a = float(input('Enter first side: '))
+b = float(input('Enter second side: '))
+c = float(input('Enter third side: '))
+
+s = (a+b+c)/2
+area = (s*(s-a)*(s-b)*(s-c)) ** 0.5
+print("The area of the triangle is %0.2f" %area)
